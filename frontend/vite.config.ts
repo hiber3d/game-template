366320ba501eeba89be9f3063982665c90d1bb73
Filip Engberg <filip.engberg@hiberworld.com>
2024-12-06 14:03:23 +0100

    Add multiple assets plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { DynamicPublicDirectory } from "vite-multiple-assets";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    DynamicPublicDirectory(
+      ["public/**", { input: "../game/assets/**", output: "/assets" }],
+      {
+        ssr: false,
+        mimeTypes: {
+          ".glb": "application/glb",
+          ".png": "image/png",
+        },
+      }
+    ),
+  ],
   server: {
     fs: {
       allow: [".."],
