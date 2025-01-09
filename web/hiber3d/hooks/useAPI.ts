8289e750303b3e39be645cedcaf2fe15487fedcb
Filip Engberg <filip.engberg@hiberworld.com>
2025-01-09 16:35:33 +0100

    Remove useAPI hook

--- web/hiber3d/hooks/useAPI.ts
+++
@@ -1,9 +0,0 @@
-// useAPI.ts
-import { Hiber3DContext } from "hiber3d/context/Hiber3DContext";
-import { useContext } from "react";
-
-export const useAPI = () => {
-  const hiber3d = useContext(Hiber3DContext);
-
-  return hiber3d?.api;
-};
