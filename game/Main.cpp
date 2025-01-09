118455b5311ab53a59773cf6d9d79ec9f84c3f1c
Per Baltzar <per.baltzar@hiberworld.com>
2025-01-09 11:33:24 +0100

    Update engine version to 1.86

--- game/Main.cpp
+++ game/Main.cpp
@@ -8,8 +8,10 @@
 #include <Hiber3D/Gltf/GltfModule.hpp>
 #include <Hiber3D/Hiber3D.hpp>
 #include <Hiber3D/Interop/InteropModule.hpp>
+#include <Hiber3D/Modules/Hierarchy/HierarchyModule.hpp>
 #include <Hiber3D/Modules/Log/LogModule.hpp>
 #include <Hiber3D/Modules/Renderer/RenderModule.hpp>
+#include <Hiber3D/Modules/WorldTransform/WorldTransformModule.hpp>
 #include <Hiber3D/Scene/SceneModule.hpp>
 
 #include <stdio.h>
@@ -19,11 +21,13 @@ public:
     std::string_view name() override { return "MainModule"; }
 
     void onRegister(Hiber3D::InitContext& context) override {
-        context.registerModule<Hiber3D::AssetModule>();
-        context.registerModule<Hiber3D::LogModule>();
-        context.registerModule<Hiber3D::RenderModule>();
+        context.registerModule<Hiber3D::AssetModule>(Hiber3D::AssetModuleSettings{.enableWatcher = false});
+        context.registerModule<Hiber3D::LogModule>(Hiber3D::LogSettings{.logLevel = Hiber3D::LogLevel::INFO});
         context.registerModule<Hiber3D::GltfModule>();
         context.registerModule<Hiber3D::SceneModule>();
+        context.registerModule<Hiber3D::WorldTransformModule>();
+        context.registerModule<Hiber3D::RenderModule>();
+        context.registerModule<Hiber3D::HierarchyModule>();
         context.getModule<Hiber3D::AssetModule>().registerAssetType<Hiber3D::Mesh>(context);
         context.getModule<Hiber3D::AssetModule>().registerAssetType<Hiber3D::StandardMaterial>(context);
         context.getModule<Hiber3D::AssetModule>().registerAssetType<Hiber3D::Cubemap>(context);
@@ -35,6 +39,6 @@ public:
 };
 
 int main(int argc, char* argv[]) {
-    Hiber3D::run("GameTemplate", std::make_unique<MainModule>(), argc, argv);
+    Hiber3D::run("Seaman", std::make_unique<MainModule>(), argc, argv);
     return 0;
 }
--- game/Modules/ExampleModule/ExampleEvent.hpp
+++ game/Modules/ExampleModule/ExampleEvent.hpp
@@ -6,5 +6,5 @@ struct ExampleEvent {
     int value;
 };
 
-HIBER3D_REFLECT(ExampleEvent, HIBER3D_MEMBER(value));
+HIBER3D_REFLECT(HIBER3D_TYPE(ExampleEvent), HIBER3D_MEMBER(value));
 HIBER3D_INTEROP_SEND_AND_RECEIVE_FROM_JS(ExampleEvent);
