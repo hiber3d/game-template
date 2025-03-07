d55e9a9b662e336abc8660e5f4143db9c455507a
Per Baltzar <per.baltzar@hiberworld.com>
2025-03-07 15:21:02 +0100

    Use new zip (#4)
    * use new zip and editor
    
    * update readme
    
    * render environment
    
    * fix tailwind
    
    * .
    
    * .
    
    ---------
    
    Co-authored-by: Filip Engberg <filip.engberg@hiberworld.com>

--- web/App.tsx
+++ web/App.tsx
@@ -1,9 +1,13 @@
-import { Hiber3D } from "hiber3d/components/Hiber3D";
+import { Hiber3D } from "@hiber3d/web";
+import { moduleFactory } from "../build/moduleFactory";
 
 export const App = () => (
-  <Hiber3D>
-    <div className="relative w-full h-dvh flex items-center justify-center">
-      <h1 className="text-white">Hello Game!</h1>
+  <Hiber3D
+    moduleFactory={moduleFactory}
+    config={{ Renderer: { Quality: 3, MSAASampleCount: 1 } }}
+  >
+    <div className="absolute w-full h-full flex items-start justify-center p-2">
+      <h1 className="text-white pt-4">Web UI in Play Mode</h1>
     </div>
   </Hiber3D>
 );
