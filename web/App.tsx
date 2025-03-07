6aad49bb4091a044c76393dfb39ea11d52f94696
perbaltzar <per.baltzar@hiberworld.com>
2025-03-07 14:34:54 +0100

    render environment

--- web/App.tsx
+++ web/App.tsx
@@ -2,7 +2,10 @@ import { Hiber3D } from "@hiber3d/web";
 import { moduleFactory } from "../build/moduleFactory";
 
 export const App = () => (
-  <Hiber3D moduleFactory={moduleFactory}>
+  <Hiber3D
+    moduleFactory={moduleFactory}
+    config={{ Renderer: { Quality: 3, MSAASampleCount: 1 } }}
+  >
     <div className="relative w-full h-dvh flex items-center justify-center">
       <h1 className="text-white">Hello Game!</h1>
     </div>
