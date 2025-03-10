9d4bca3751995811107280d974883e2e942aee06
Filip Engberg <filip.engberg@hiberworld.com>
2025-03-10 16:29:27 +0100

    Fix build and styling

--- web/App.tsx
+++ web/App.tsx
@@ -1,11 +1,7 @@
 import { Hiber3D } from "@hiber3d/web";
-import { moduleFactory } from "../build/moduleFactory";
 
 export const App = () => (
-  <Hiber3D
-    moduleFactory={moduleFactory}
-    config={{ Renderer: { Quality: 3, MSAASampleCount: 1 } }}
-  >
+  <Hiber3D>
     <div className="absolute w-full h-full flex items-start justify-center p-2">
       <h1 className="text-white pt-4">Web UI in Play Mode</h1>
     </div>
