ee04cf39ac6ebb14b2ed780f6422936fc160def2
John Segerstedt <john.segerstedt@hiberworld.com>
2025-03-25 15:29:17 +0100

    minor tweaks

--- assets/scripts/gun.js
+++ assets/scripts/gun.js
@@ -10,13 +10,15 @@
 
     update(dt) {
       if (hiber3d.call("keyJustPressed", KEYS.FIRE)) {
-        // Create bullet entity
-        const worldTransform = hiber3d.getValue(this.entity, "Hiber3D::ComputedWorldTransform");
+        // Create bullet
         const bulletEntity = hiber3d.createEntity();
+
         hiber3d.addComponent(bulletEntity, "Hiber3D::SceneRoot");
         hiber3d.setValue(bulletEntity, "Hiber3D::SceneRoot", "scene", BULLET_SCENE);
+
         hiber3d.addComponent(bulletEntity, "Hiber3D::Transform");
-        hiber3d.setValue(bulletEntity, "Hiber3D::Transform", worldTransform);
+        hiber3d.setValue(bulletEntity, "Hiber3D::Transform", hiber3d.getValue(this.entity, "Hiber3D::ComputedWorldTransform"));
+
         hiber3d.addComponent(bulletEntity, "Hiber3D::Name");
         hiber3d.setValue(bulletEntity, "Hiber3D::Name", "Bullet");
       }
