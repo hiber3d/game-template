15a3a3adb9cd7a603dbe651dffc46b654a756185
Filip Engberg <filip.engberg@hiberworld.com>
2025-05-07 13:32:11 +0200

    Update tutorial
    - Use Web UI instead of RML
    - Link to docs in tutorial.md

--- web/App.tsx
+++ web/App.tsx
@@ -1,18 +1,19 @@
-import { Hiber3D, useHiber3D } from "@hiber3d/web";
+import { GunStateChangedEvent, Hiber3D, useHiber3D } from "@hiber3d/web";
 import { moduleFactory as webGPU } from "GameTemplate_webgpu";
 import { moduleFactory as webGL } from "GameTemplate_webgl";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const ExampleEvent = () => {
+const ExampleUI = () => {
   const { api } = useHiber3D();
+  const [gunState, setGunState] = useState<GunStateChangedEvent | undefined>();
 
   useEffect(() => {
     if (!api) {
       return;
     }
 
-    const listener = api.onExampleEvent((payload) => {
-      console.debug(payload);
+    const listener = api.onGunStateChangedEvent((payload) => {
+      setGunState(payload);
     });
 
     return () => {
@@ -20,11 +21,36 @@ const ExampleEvent = () => {
     };
   }, [api]);
 
-  return null;
+  if (!gunState) {
+    return null;
+  }
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        pointerEvents: "none",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "flex-start",
+        color: "white",
+        padding: "12px 25px",
+        fontSize: "20px",
+        fontWeight: "bold",
+      }}
+    >
+      <div>AMMO: {gunState.ammo}</div>
+      {/* <div>HITS: {gunState.hits}</div> */}
+    </div>
+  );
 };
 
 export const App = () => (
-  <Hiber3D build={{ webGPU, webGL }}>
-    <ExampleEvent />
+  <Hiber3D build={{ webGPU, webGL }} config={{ Editor: { Enabled: false, Mode: "play" } }}>
+    <ExampleUI />
   </Hiber3D>
 );
