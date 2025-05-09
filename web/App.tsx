683e6c8738ce77fd08546e04dc86b08bbaffcc55
Filip Engberg <filip.engberg@hiberworld.com>
2025-05-09 14:35:38 +0200

    Release 0.110.0 (#31)
    * Use createHiber3DApp
    
    * Update engine version
    
    * Remove editor settings
    
    * Update cmake list
    
    ---------
    
    Co-authored-by: mikryd <mikryding@gmail.com>

--- web/App.tsx
+++ web/App.tsx
@@ -1,7 +1,5 @@
-import { Hiber3D, useHiber3D } from "@hiber3d/web";
-import { moduleFactory as webGPU } from "GameTemplate_webgpu";
-import { moduleFactory as webGL } from "GameTemplate_webgl";
 import { useEffect } from "react";
+import { Hiber3D, useHiber3D } from "./hiber3d";
 
 const ExampleEvent = () => {
   const { api } = useHiber3D();
@@ -24,7 +22,7 @@ const ExampleEvent = () => {
 };
 
 export const App = () => (
-  <Hiber3D build={{ webGPU, webGL }}>
+  <Hiber3D>
     <ExampleEvent />
   </Hiber3D>
 );
