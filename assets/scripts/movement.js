54a8102ef81b04b3f03a1521e7fd1426501aaec0
John Segerstedt <john.segerstedt@hiberworld.com>
2025-06-09 11:40:00 +0200

    [HIB-34527] Updates GameTemplate to follow new QuickJS scripting

--- assets/scripts/movement.js
+++ assets/scripts/movement.js
@@ -1,42 +1,42 @@
-/**
- * This script handles keyboard movement for the entity it is attached to.
- */
-const MOVEMENT_KEYS = {
-  FORWARD: 41, // W
-  BACKWARD: 37, // S
-  STRAFE_LEFT: 19, // A
-  STRAFE_RIGHT: 22, // D
-};
+//  This script handles keyboard movement for the entity it is attached to.
 
-const MOVEMENT_SPEED = 10; // meters per second
+export default class {
 
-({
-  onCreate() { },
+  MOVEMENT_KEYS = {
+    FORWARD: 41, // W
+    BACKWARD: 37, // S
+    STRAFE_LEFT: 19, // A
+    STRAFE_RIGHT: 22, // D
+  };
 
-  update(dt) {
+  MOVEMENT_SPEED = 10; // meters per second
+
+  onCreate() { }
+
+  onUpdate(dt) {
     // Get the transform component
-    const transform = hiber3d.getValue(this.entity, "Hiber3D::Transform");
+    const transform = hiber3d.getComponent(this.entity, "Hiber3D::Transform");
 
     // Update position based on the keys pressed
     var toMove = { x: 0, y: 0, z: 0 };
-    if (hiber3d.call("keyIsPressed", MOVEMENT_KEYS.FORWARD)) {
-      toMove.z -= dt * MOVEMENT_SPEED;
+    if (hiber3d.call("keyIsPressed", this.MOVEMENT_KEYS.FORWARD)) {
+      toMove.z -= dt * this.MOVEMENT_SPEED;
     }
-    if (hiber3d.call("keyIsPressed", MOVEMENT_KEYS.BACKWARD)) {
-      toMove.z += dt * MOVEMENT_SPEED;
+    if (hiber3d.call("keyIsPressed", this.MOVEMENT_KEYS.BACKWARD)) {
+      toMove.z += dt * this.MOVEMENT_SPEED;
     }
-    if (hiber3d.call("keyIsPressed", MOVEMENT_KEYS.STRAFE_LEFT)) {
-      toMove.x -= dt * MOVEMENT_SPEED;
+    if (hiber3d.call("keyIsPressed", this.MOVEMENT_KEYS.STRAFE_LEFT)) {
+      toMove.x -= dt * this.MOVEMENT_SPEED;
     }
-    if (hiber3d.call("keyIsPressed", MOVEMENT_KEYS.STRAFE_RIGHT)) {
-      toMove.x += dt * MOVEMENT_SPEED;
+    if (hiber3d.call("keyIsPressed", this.MOVEMENT_KEYS.STRAFE_RIGHT)) {
+      toMove.x += dt * this.MOVEMENT_SPEED;
     }
     const toMoveRotated = hiber3d.call("quaternionRotateDirection", transform.rotation, toMove);
     transform.position = {x: transform.position.x + toMoveRotated.x, y: transform.position.y + toMoveRotated.y, z: transform.position.z + toMoveRotated.z};
     
     // Get updated transform component
-    hiber3d.setValue(this.entity, "Hiber3D::Transform", transform);
-  },
+    hiber3d.setComponent(this.entity, "Hiber3D::Transform", transform);
+  }
 
-  onEvent(event, payload) { },
-});
+  onEvent(event, payload) { }
+}
